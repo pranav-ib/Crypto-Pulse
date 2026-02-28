e858b551e23b6d74991b0832571645bae3462b6d
Pranav I B <pranavbabunadhan@gmail.com>
2026-02-28 16:39:15 +0530

    card alignment in watchlist

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,38 @@
-function Card( { coin, onToggleWatchlist, isWatchlisted }) {
-    const isPositive = coin.price_change_24h >= 0;
+function Card({ coin, onToggleWatchlist, isWatchlisted }) {
+  const isPositive = coin.price_change_percentage_24h >= 0;
 
-    return (
-        <div className="card">
-            <div className="card-header">
-                <img src= {coin.image} alt = {coin.name} />
-                <div>
-                    <h3>{coin.name}</h3>
-                    <p className="symbol"> {coin.symbol.toUpperCase()}</p>
-                </div>
-                <button
-                    className="watch-btn"
-                    onClick={() => onToggleWatchlist?.(coin)}
-                    >
-                    {isWatchlisted ? "★" : "☆"}
-                </button>
-            </div>
+  return (
+    <div className="card">
+      <button
+        className="watch-btn"
+        onClick={() => onToggleWatchlist?.(coin)}
+      >
+        {isWatchlisted ? "★" : "☆"}
+      </button>
 
-            <div className="crypto-body">
-                <p className="price">${coin.current_price}</p>
-                <p className={isPositive ? "change positive" : "change negative"}>
-                {coin.price_change_percentage_24h?.toFixed(2)}%
-                </p>
-            </div>
+      
+      <div className="card-header">
+        <img src={coin.image} alt={coin.name} />
+
+        <div>
+          <h3 className="coin-name">{coin.name}</h3>
+          <p className="symbol">{coin.symbol.toUpperCase()}</p>
         </div>
-    );
+      </div>
 
+      
+      <div className="card-body">
+        <p className="price">
+          ${coin.current_price.toLocaleString()}
+        </p>
 
+        <p className={isPositive ? "change positive" : "change negative"}>
+          {isPositive ? "↗" : "↘"}{" "}
+          {coin.price_change_percentage_24h?.toFixed(2)}%
+        </p>
+      </div>
+    </div>
+  );
 }
+
 export default Card;
